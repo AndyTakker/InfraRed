143f7884a8a1b46776527935734dd966a5c180cc
AndyTakker <andrei.kolesov@gmail.com>
2026-01-19 20:20:35 +0300

    Initial

---
+++ examples/InfraRedSample.cpp
@@ -0,0 +1,79 @@
+//==============================================================================
+// Пример работы с IR-приемником на CH32V003.
+// IR-приемник подключается к порту PD4.
+//------------------------------------------------------------------------------
+#include <InfraRed.h>
+#include <SysClock.h>
+#include <debug.h>
+
+InfraRed ir; // Объект, отвечающий за прием команд с IR-пульта
+
+//==============================================================================
+// Настройка прерывания
+//------------------------------------------------------------------------------
+void EXTI_INIT(void) {
+  GPIO_InitTypeDef GPIO_InitStructure = {0};
+  EXTI_InitTypeDef EXTI_InitStructure = {0};
+  NVIC_InitTypeDef NVIC_InitStructure = {0};
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOD, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4; // PD4
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+  // GPIOD ----> EXTI_Line4
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource4);
+  EXTI_InitStructure.EXTI_Line = EXTI_Line4;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
+
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI7_0_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void EXTI7_0_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI7_0_IRQHandler(void) {
+  if (EXTI_GetITStatus(EXTI_Line4) != RESET) { // На PD4 сигнал изменился из 1 в 0
+    ir.receive();                              // Обработчик приема пакета
+    EXTI_ClearITPendingBit(EXTI_Line4);
+  }
+}
+#ifdef __cplusplus
+}
+#endif
+
+//==============================================================================
+int main() {
+  // Стандартные приседания
+  SystemCoreClockUpdate();
+
+  SysClock sc;    // Подключаем библиотеку работы с временными интервалами
+  sc.begin();     // Инициализируем библиотеку
+  sc.delay(4000); // Ждем, пока раздуплится терминал в VSCode
+
+  USART_Printf_Init(115200);
+  printf("SystemClk: %lu\r\n", SystemCoreClock);      // Для посмотреть частоту процесора (48мГц)
+  printf("   ChipID: %08lx\r\n", DBGMCU_GetCHIPID()); // Для посмотреть ID чипа, от нефиг делать
+
+  EXTI_INIT(); // Настройка прерываний
+
+  while (1) {
+    // Ждем приема пакета из IR-порта
+    if (ir.ready()) { // если пакет принят
+      printf("Packet: %08lX, Command: %02X, Address: %02X \r\n", ir.readPacket(), ir.readCommand(), ir.readAddress());
+      // Если нужно, сюда можно добавить проверку принятых данных с помощью функций:
+      // ir.chkParity(); ir.chkAddress(); ir.chkCommand();
+      // Что считать правильным - решать индивидуально с учетом конкретного пульта.
+    }
+  }
+}
---
+++ src/InfraRed.h
@@ -0,0 +1,62 @@
+//==============================================================================
+// Библиотека для работы с ИК протоколом NEC.
+// Идея почерпнута у Гайвера: https://github.com/GyverLibs/NecDecoder
+// Библиотека реализована для работы на CH32V003 без Arduino-framework.
+// Поскольку куча пультов поддерживает протокол NEC не полностью, в
+// библиотеке реализован подход: сигнал принимается как есть, а проверки
+// корректности пакета выполняются на прикладном уровне в зависимости от
+// конкретного используемого пульта.
+// Для этого в библиотеке есть необходимые функции: проверка четности,
+// корректность адреса, корректность команды.
+// Временные параметры сигнала используем NEC.
+//
+// Особенности:
+// - функция receive() должна вызываться в прерывании от входного сигнала
+//   c IR-приемника по переходу из высокого уровня в низкий.
+// - для расчета длительности импульсов используется счетчик таймера TIM2.
+// - режим повтора не реализован (ни разу не потребовался).
+//------------------------------------------------------------------------------
+#ifndef INFRARED_H
+#define INFRARED_H
+
+#include <ch32v00x.h>
+
+#define _NEC_PACKET_SIZE 32  // размер пакета (бит)
+#define _NEC_TOLERANCE 150   // допуск high/low, мкс
+#define _NEC_TOLERANCE2 1500 // допуск start/repeat, мкс
+
+// Тайминги NEC, мкс
+#define _NEC_HIGH_BIT 2250
+#define _NEC_LOW_BIT 1150
+#define _NEC_START_BIT 14400
+
+// =========================================================================
+#define _NEC_HIGH_MIN (_NEC_HIGH_BIT - _NEC_TOLERANCE)
+#define _NEC_HIGH_MAX (_NEC_HIGH_BIT + _NEC_TOLERANCE)
+#define _NEC_LOW_MIN (_NEC_LOW_BIT - _NEC_TOLERANCE)
+#define _NEC_LOW_MAX (_NEC_LOW_BIT + _NEC_TOLERANCE)
+#define _NEC_START_MIN (_NEC_START_BIT - _NEC_TOLERANCE2)
+#define _NEC_START_MAX (_NEC_START_BIT + _NEC_TOLERANCE2)
+
+class InfraRed {
+  public:
+  InfraRed();
+  void receive();        // Вызывать при переходе из High в Low (Falling) на пине ИК приемника в прерывании
+  bool ready();          // Возвращает true, если пакет прочитан
+  uint32_t readPacket(); // Прочитать пакет целиком (адрес + ~адрес + команда + ~команда)
+  uint8_t readAddress(); // Прочитать адрес
+  uint8_t readCommand(); // Прочитать команду
+  bool chkParity();      // Проверка четности пакета
+  bool chkAddress();     // Проверка адреса
+  bool chkCommand();     // Проверка команды
+
+  private:
+  void timerInit(); // Настройка TIM2 для расчета интервалов времени между фронтами импульсов
+
+  volatile uint32_t _packet = 0;               // Буфер последнего принятого пакета (4 байта)
+  volatile uint32_t _buffer = 0;               // Буфер текущего принимаемого пакета (4 байта)
+  volatile int8_t _counter = _NEC_PACKET_SIZE; // Счетчик бит в принимаемом пакете
+  volatile bool _ready = false;                // Флаг готовности данных к чтению
+  volatile bool _start = false;                // старт флаг
+};
+#endif
