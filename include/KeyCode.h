13469a1fb78f95aa36a6f56d15c8bfd5223122c6
AndyTakker <andrei.kolesov@gmail.com>
2026-02-10 00:00:55 +0300

    Добавлено описание кодов клавиш популярных пультов

---
+++ include/KeyCode.h
@@ -0,0 +1,65 @@
+// Определение кодов клавиш китайских пультов
+
+// 1. "Классический" пульт 
+#define irPower 0xA2
+#define irMode 0x62
+#define irMute 0xE2
+#define irPlay 0x22 
+#define irBack 0x02
+#define irFwd 0xC2     
+#define irEQ 0xE0      
+#define irKeyPlus 0x90 
+#define irKeyMinus 0xA8
+#define irRand 0x98
+#define irScan 0xB0
+#define irKey0 0x68
+#define irKey1 0x30
+#define irKey2 0x18
+#define irKey3 0x7A
+#define irKey4 0x10
+#define irKey5 0x38
+#define irKey6 0x5A
+#define irKey7 0x42
+#define irKey8 0x4A
+#define irKey9 0x52
+
+// 2. Пульт "цифры и курсор"
+#define irKey1 0xA2
+#define irKey2 0x62
+#define irKey3 0xE2
+#define irKey4 0x22
+#define irKey5 0x02
+#define irKey6 0xC2
+#define irKey7 0xE0
+#define irKey8 0xA8
+#define irKey9 0x90
+#define irKey0 0x98
+#define irKeyStar 0x68
+#define irKeyHash 0xB0
+#define irOk 0x38
+#define irBack 0x10
+#define irFwd 0x5A
+#define irUp 0x18
+#define irDown 0x4A
+
+// 3. Пульт "питание, меню, тест, курсор и цифры"
+#define irPower 0xA2
+#define irMenu 0xE2
+#define irTest 0x22
+#define irPlus 0x02
+#define irRestore 0xc2
+#define irBack 0xE0
+#define irPlay 0xA8
+#define irFwd 0x90
+#define irKey0 0x68
+#define irMinus 0x98
+#define irKeyC 0xB0
+#define irKey1 0x30
+#define irKey2 0x18
+#define irKey3 0x7A
+#define irKey4 0x10
+#define irKey5 0x38
+#define irKey6 0x5A
+#define irKey7 0x42
+#define irKey8 0x4A
+#define irKey9 0x52
